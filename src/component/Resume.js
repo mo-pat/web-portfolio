8bac1f2f026df8c5d1f0c6ee51c44539df838664
mo-jinbuu <mohit-patel7@hotmail.com>
2020-11-11 20:51:56 -0500

    update resume

--- src/component/Resume.js
+++ src/component/Resume.js
@@ -4,20 +4,20 @@ import Education from './Education.js'
 export const Resume = () => {
   return (
     <div className="resume-background flex justify-center items-center bg-blue-800 py-12" id="resume">
-      <div className="resume bg-white shadow-lg w-full md:mt-2 mx-6 md:mx-32 rounded">
+      <div className="resume bg-white shadow-lg w-full md:mt-2 mx-6 md:mx-16 rounded">
         <header className="resume-header mt-12 mx-6 md:mx-12 pb-4 text-left text-3xl border-b-2 border-yellow-600">
           Qualifications
         </header>
-        <div className="qualification-list pl-8 md:pl-24 py-8">
+        <div className="qualification-list pl-8 md:pl-16 py-8">
           <ul className="list-disc space-y-2 font-sans">
             <li>
-              Skilled with the following languages: JavaScript (Proficient), Python, C/C++, HTML, CSS, FORTRAN
+              Proficient with the following languages: JavaScript, Python, C/C++, HTML, CSS, FORTRAN
             </li>
             <li>
-              Good understanding of modern javacsript (ES6+) and writting code using its latest features (arrow functions, template literals, destructuring, async await)
+              Good understanding of modern javascript (ES6+) and writting code using its latest features (arrow functions, template literals, destructuring, async await)
             </li>
             <li>
-              Strong understanding of how the web works and client-server architecture.
+              Good understanding of the modern web architecture.
             </li>
             <li>
               Adept front end developer, competent with the following frameworks/libraries: React, Tailwind CSS. Able to create modern UI/UX designs that are responsive and dynamic.
@@ -29,7 +29,7 @@ export const Resume = () => {
               Knowledge of SQL and NoSQL databases (Postgres, MongoDB)
             </li>
             <li>
-              Strong understanding of writting code that follow general software design principles (DRY, KISS, Abstraction, etc.)
+              Strong understanding of writting code that follows general software design principles (DRY, KISS, Abstraction, etc.)
             </li>
             <li>
               Experience using bash and working in a Linux environment
@@ -41,13 +41,13 @@ export const Resume = () => {
               Working with an engineering mindset; providing solutions that work within constraints and with scalability in mind
             </li>
             <li>
-              Newcomer to the open source community; participated in Hacktoberfest 2020
+              Active in the open source community; participated in Hacktoberfest 2020
             </li>
             <li>
               Active learner; keeping up with the latest web technologies through daily.dev articles
             </li>
             <li>
-              Ability to work in muitidisciplinary groups while also be self-autonomous
+              Ability to work in muitidisciplinary groups while also being self-autonomous
             </li>
           </ul>
         </div>
